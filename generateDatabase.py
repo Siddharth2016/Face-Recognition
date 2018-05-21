bf249eb8273a241d43c6c1843fb7ebd0c37f0f26
Siddharth2016 <siddharthchandragzb@gmail.com>
2018-05-21 18:52:57 +0530

    generating database for 150 entries, one set at a time

--- generateDatabase.py
+++ generateDatabase.py
@@ -8,9 +8,9 @@ cap = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier("./cascades/haarcascade_frontalface_default.xml")
 
-count = 40
+count = 0
 
-while True and count<51:
+while True and count<151:
 
     _, frame = cap.read()
     grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -19,7 +19,7 @@ while True and count<51:
 
     for (x,y,w,h) in faces:
 
-        roi = grayframe[y-20:y+h+20, x-20:x+h+20]
+        roi = grayframe[y:y+h, x:x+h]
         roi = cv2.resize(roi, (200,200))
 
         cv2.imwrite("./database/{}.pgm".format(count), roi)
